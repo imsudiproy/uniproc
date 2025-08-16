824a3bbef742325288399d2f9fe57148fd5d737d
Sudip Roy <sudiproy1999@gmail.com>
2025-08-17 01:47:37 +0530

    add cpu info

--- src/datasources/cpu_mem.rs
+++ src/datasources/cpu_mem.rs
@@ -12,6 +12,15 @@ pub fn print_system_info(){
     println!("used memory : {} bytes", sys.used_memory());
     println!("total swap  : {} bytes", sys.total_swap());
     println!("used swap   : {} bytes", sys.used_swap());
+
+    // Display system information:
+    println!("System name:             {:?}", System::name());
+    println!("System kernel version:   {:?}", System::kernel_version());
+    println!("System OS version:       {:?}", System::os_version());
+    println!("System host name:        {:?}", System::host_name());
+
+// Number of CPUs:
+println!("NB CPUs: {}", sys.cpus().len());
 }
 
 

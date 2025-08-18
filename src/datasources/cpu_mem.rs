926d7b45b0ff2789fc507be206a16504c24b6b41
Sudip Roy <sudiproy1999@gmail.com>
2025-08-19 02:15:15 +0530

    remove the crate code

--- src/datasources/cpu_mem.rs
+++ src/datasources/cpu_mem.rs
@@ -1,6 +1,4 @@
 use core::time;
-// use std::os::unix::thread;
-// use std::thread::sleep;
 use std::thread;
 
 use sysinfo::{System, Pid};
@@ -31,27 +29,3 @@ pub fn show_all_process (pid: u32, interval: u64, duration: Option<u64>) {
 
     }
 }
-
-// pub fn monitor_pid(pid : u32){
-    
-//     let mut sys = System::new_all();
-//     sys.refresh_all();
-
-//     println!("=> system:");
-// // RAM and swap information:
-//     println!("total memory: {} bytes", sys.total_memory());
-//     println!("used memory : {} bytes", sys.used_memory());
-//     println!("total swap  : {} bytes", sys.total_swap());
-//     println!("used swap   : {} bytes", sys.used_swap());
-
-//     // Display system information:s
-//     println!("System name:             {:?}", System::name());
-//     println!("System kernel version:   {:?}", System::kernel_version());
-//     println!("System OS version:       {:?}", System::os_version());
-//     println!("System host name:        {:?}", System::host_name());
-
-// // Number of CPUs:
-// println!("NB CPUs: {}", sys.cpus().len());
-// }
-
-
